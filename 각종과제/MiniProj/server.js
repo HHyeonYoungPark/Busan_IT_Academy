359bfc66f2ce0052bd6953c674e5fc096780dbe5
HyeonYoung PARK <103189529+HHyeonYoungPark@users.noreply.github.com>
2022-09-29 16:29:44 +0900

    Add files via upload

--- 각종과제/MiniProj/server.js
+++ 각종과제/MiniProj/server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   res.render("main", { title, subTitle: " > Main" });
 });
 
-// 회원관리 페이지(회원목록)
+// 회원관리 페이지 (회원목록)
 app.get("/member", (req, res) => {
   let sql =
     "SELECT idx, username, userid, email, grade, regdate FROM member ORDER BY idx DESC;";
@@ -42,7 +42,18 @@ app.get("/member", (req, res) => {
     if (err) {
       throw err;
     }
-    res.render("member", { result });
+    res.render("member", { result, title, subTitle: " > Member" });
+  });
+});
+
+// 회원 등급 변경
+app.post("/gradeUpdate", (req, res) => {
+  let sql = "UPDATE member SET grade = ? WHERE idx = ?;";
+  db.query(sql, [req.body.gradeValue, req.body.idValue], (err) => {
+    if (err) {
+      throw err;
+    }
+    res.send({ status: "success", grade: req.body.gradeValue });
   });
 });
 
@@ -84,7 +95,7 @@ app.get("/upUser/:idx", (req, res) => {
     if (err) {
       throw err;
     }
-    res.render("upUser", { result });
+    res.render("upUser", { result, title, subTitle: " > Member > Update" });
   });
 });
 
@@ -121,13 +132,100 @@ app.get("/userView/:idx", (req, res) => {
     if (err) {
       throw err;
     }
-    res.render("userView", { result });
+    res.render("userView", { result, title, subTitle: " > Member > View" });
   });
 });
 
-// 게시판관리 페이지
+// 회원 검색
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+// 게시판관리 페이지 (게시글 목록)
 app.get("/board", (req, res) => {
-  res.render("board", { title, subTitle: " > Board" });
+  let sql =
+    "SELECT idx, title, username, visit, regdate FROM board ORDER BY idx DESC;";
+  db.query(sql, (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.render("board", { result, title, subTitle: " > Board" });
+  });
 });
+
+// 게시글 작성
+app.get("/addPost", (req, res) => {
+  res.render("addPost", { title, subTitle: " > Board > AddPost" });
+});
+
+app.post("/addPost", (req, res) => {
+  let sql = "INSERT INTO board VALUES(NULL, ?, ?, ?, 1, now())";
+  db.query(
+    sql,
+    [req.body.title, req.body.username, req.body.content],
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      res.redirect("/board");
+    }
+  );
+});
+
+// 게시글 삭제
+app.get("/delPost/:idx", (req, res) => {
+  let sql = "DELETE FROM board WHERE idx = ?;";
+  db.query(sql, [req.params.idx], (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(req.params.idx + "번 게시글이 삭제되었습니다.");
+    res.redirect("/board");
+  });
+});
+
+// 게시글 수정
+app.get("/upPost/:idx", (req, res) => {
+  let sql = "SELECT * FROM board WHERE idx = ?;";
+  db.query(sql, [req.params.idx], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.render("upPost", { result, title, subTitle: " > Board > Update" });
+  });
+});
+
+app.post("/upPost", (req, res) => {
+  let sql = "UPDATE board SET ";
+  sql += "title=?, username=?, content=?, regdate=now() ";
+  sql += "WHERE idx = ?;";
+
+  const post = {
+    idx: req.body.idx,
+    title: req.body.title,
+    username: req.body.username,
+    content: req.body.content,
+  };
+
+  db.query(sql, [post.title, post.username, post.content, post.idx], (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log(req.body.idx + "번 게시글이 수정되었습니다.");
+    res.redirect("/board");
+  });
+});
+
+// 게시글 상세보기
+app.get("/postView/:idx", (req, res) => {
+  let viewSQL = "SELECT * FROM board WHERE idx = ?;";
+  db.query(viewSQL, [req.params.idx], (err, result) => {
+    if (err) {
+      throw err;
+    }
+    res.render("postView", { result, title, subTitle: " > Board > View" });
+  });
+});
+
+// 게시글 검색
 
 app.listen(PORT, () => console.log(`Server Running...localhost:${PORT}`));
